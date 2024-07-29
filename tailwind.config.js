e31de32198a120e9ed0218d224bde105ebb5c54d
Square <aayushk4980@gmail.com>
2024-07-29 16:19:33 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'product-card-shadow': '0 5px 15px rgba(0, 0, 0, 0.35)',
+        'product-card-shadow-hover': '0 1px 4px rgba(0, 0, 0, 0.16), 0 0 0 2px #333',
+      },
+    },
   },
   plugins: [],
 }
