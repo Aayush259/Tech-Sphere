5a787bac4ff4bb8089cc113ba2ea452d193ad5b0
Square <aayushk4980@gmail.com>
2024-08-08 19:00:01 +0530

    Added wishlist reducers

---
+++ src/app/reducers/wishlistReducers.js
@@ -0,0 +1,20 @@
+
+// Reducer function to add items in wishlist.
+const add = (state, action) => {
+    // Getting item index.
+    const itemIndex = state.value.findIndex(item => item.id === action.payload.id);
+
+    // If item is not present in wishlist, add it to wishlist.
+    if (itemIndex === -1) {
+        state.value = [...state.value, { ...action.payload }];
+    };
+};
+
+// Reducer function to remove items from wishlist.
+const remove = (state, action) => {
+    const prevItems = [...state.value];
+
+    state.value = prevItems.filter(item => item.id === action.payload);
+};
+
+export { add, remove };
