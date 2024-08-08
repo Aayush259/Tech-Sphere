4b0105bae98429d4849826089b9be9964b093426
Square <aayushk4980@gmail.com>
2024-08-09 00:22:27 +0530

    Fixed navigation links styling

--- src/components/navbar/NavigationLink.jsx
+++ src/components/navbar/NavigationLink.jsx
@@ -7,7 +7,7 @@ export default function NavigationLink({ linkText, linkTo, setHamActive }) {
     return (
         <NavLink
             to={linkTo}
-            className="w-full text-center relative py-3 xl:w-fit xl:py-0 before:block before:absolute before:h-[2px] before:w-0 sm:before:bg-white before:rounded before:-bottom-1 before:left-1/2 sm:hover:before:w-full sm:hover:before:left-0 before:duration-300 duration-300"
+            className={({ isActive }) => `${isActive ? "before:w-full before:left-0" : ""} w-fit text-center relative py-3 xl:w-fit xl:py-0 before:block before:absolute before:h-[2px] before:w-0 before:bg-white before:rounded before:bottom-2 xl:before:-bottom-1 before:left-1/2 sm:hover:before:w-full sm:hover:before:left-0 before:duration-300 duration-300`}
             onClick={() => setHamActive(prevHamState => !prevHamState)}
         >
             {linkText}
