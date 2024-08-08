902e683c72731af009c5e63c1883d18fd4c70106
Square <aayushk4980@gmail.com>
2024-08-08 10:53:14 +0530

    Created cart reducers

---
+++ src/app/reducers/cartReducers.js
@@ -0,0 +1,49 @@
+
+// Reducer function to add item in cart.
+const addItem = (state, action) => {
+    // Getting item index, to check whether the item is already in the cart or not.
+    const itemIndex = state.value.findIndex(item => item.id === action.payload.id);
+
+    // If the item is already in cart, update its count, otherwise add the item in cart.
+    if (itemIndex === -1) {
+        state.value = [...state.value, {...action.payload, count: 1}];
+    } else {
+        const updatedItems = [...state.value];
+        updatedItems[itemIndex]['count'] += 1;
+        state.value = updatedItems;
+    };
+};
+
+// Reducer function to remove item from cart.
+const removeItem = (state, action) => {
+    const prevItems = [...state.value];
+
+    state.value = prevItems.filter(item => item.id !== action.payload);
+};
+
+// Reducer function to increment item count in cart.
+const incrementCount = (state, action) => {
+    // Getting item index.
+    const itemIndex = state.value.findIndex(item => item.id === action.payload);
+
+    // If item is present in cart, increment its count.
+    if (itemIndex !== -1) {
+        const updatedItems = [...state.value];
+        updatedItems[itemIndex]['count'] += 1;
+        state.value = updatedItems;
+    };
+};
+
+// Reducer function to decrement item count in cart.
+const decrementCount = (state, action) => {
+    // Getting item index.
+    const itemIndex = state.value.findIndex(item => item.id === action.payload);
+    const updatedItems = [...state.value];
+
+    // If item is present in cart, decrement its count and filter out those items whose count is zero.
+    if (itemIndex !== -1) {
+        updatedItems[itemIndex]['count'] -= 1;
+        updatedItems = updatedItems.filter(item => item.count > 0);
+        state.value = updatedItems;
+    };
+};
