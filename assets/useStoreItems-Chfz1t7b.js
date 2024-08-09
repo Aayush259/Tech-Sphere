b2abad89b321ffa8978098388ce931b6ece6125b
Aayush259 <Aayush259@users.noreply.github.com>
2024-08-09 14:21:15 +0000

    deploy: a008556d8a1a65d874daf683b214c0708684c75d

--- assets/useStoreItems-Chfz1t7b.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-IYNABAAa.js";const r=()=>{const{value:s,status:u}=t(e=>e.product);return{products:s,status:u}},a=()=>t(s=>s.wishlist.value);export{a,r as u};
---
+++ assets/useStoreItems-smfyOV19.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-BEh0Rvg-.js";const r=()=>{const{value:s,status:u}=t(e=>e.product);return{products:s,status:u}},a=()=>t(s=>s.wishlist.value);export{a,r as u};
