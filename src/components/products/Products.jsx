ce473f56f004867c8080c42ad6fddaf7fd66d55a
Square <aayushk4980@gmail.com>
2024-08-09 18:51:03 +0530

    Added search feature

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,8 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useCallback, useState } from 'react';
 import { useProducts } from '../../hooks/useStoreItems.js';
 import StatusCode from '../../app/utlis/StatusCode.js';
 import Loader from '../loader/Loader.jsx';
+import Search from './Search.jsx';
 const ProductCard = lazy(() => import('./ProductCard.jsx'));
 const Error = lazy(() => import('../error/Error.jsx'));
 
@@ -9,6 +10,20 @@ export default function Products() {
 
     // Getting products from store.
     const { products, status } = useProducts();
+
+    // Products to display on screen.
+    const [productsToDisplay, setProductsToDisplay] = useState([...products]);
+
+    // Function to display products which are searched by user.
+    const search = useCallback((name) => {
+        // If the name is empty, or there is no name, then display all products, else show searched products.
+        if (!name) {
+            setProductsToDisplay(products);
+        } else {
+            const searchedProducts = products.filter(product => product['name'].toLowerCase().startsWith(name.toLowerCase()));
+            setProductsToDisplay(searchedProducts);
+        }
+    }, [products, setProductsToDisplay]);
 
     switch (status) {
         case StatusCode.LOADING:
@@ -23,17 +38,28 @@ export default function Products() {
 
         default:
             return (
-                <div
-                    className="flex flex-row flex-wrap items-stretch justify-center gap-10 my-10 lg:gap-20"
-                >
-                    <Suspense fallback={<Loader />}>
-                        {
-                            products.map(product => (
-                                <ProductCard key={product['id']} productDetails={product} />
-                            ))
-                        }
-                    </Suspense>
-                </div>
+                <>
+                    <Search search={search} />
+                    <div
+                        className="flex flex-row flex-wrap items-stretch justify-center gap-10 mt-5 mb-10 lg:gap-20"
+                    >
+                        <Suspense fallback={<Loader />}>
+                            {
+                                productsToDisplay.length > 0 ? (
+                                    productsToDisplay.map(product => (
+                                        <ProductCard key={product['id']} productDetails={product} />
+                                    ))
+                                ) : (
+                                    <p
+                                        className="text-2xl font-semibold text-indigo-900"
+                                    >
+                                        No products found
+                                    </p>
+                                )
+                            }
+                        </Suspense>
+                    </div>
+                </>
             );
     };
 };
--- src/components/products/Search.jsx
+++ src/components/products/Search.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Search() {
+export default function Search({ search }) {
+
+    const inputRef = useRef();
 
     return (
         <div
@@ -13,9 +15,12 @@ export default function Search() {
                     id="search"
                     placeholder="Type here to search"
                     className="py-1 px-2 border border-indigo-800 rounded-l-lg max-w-[70vw]"
+                    ref={inputRef}
+                    onChange={() => search(inputRef.current.value)}
                 />
                 <button
-                className="py-1 px-2 border border-l-0 border-indigo-950 rounded-r-lg bg-indigo-950 text-white hover:bg-indigo-700 duration-200"
+                    className="py-1 px-2 border border-l-0 border-indigo-950 rounded-r-lg bg-indigo-950 text-white hover:bg-indigo-700 duration-200"
+                    onClick={() => search(inputRef.current.value)}
                 >
                     <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
                 </button>
